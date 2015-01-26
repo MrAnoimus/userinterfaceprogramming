7e38b3c16240728fae866f6d3c1d003d062fd639
raynoldtanyx@gmail.com <raynoldtanyx@gmail.com@91d24cd4-7f8e-6a0e-cb8f-02ae7bcda567>
2015-01-26 16:48:12 +0000

    LMAO

--- trunk/UIProgramming/Assets/Scripts/RandomCar.cs
+++ trunk/UIProgramming/Assets/Scripts/RandomCar.cs
@@ -5,6 +5,8 @@ public class RandomCar : MonoBehaviour {
 
 	public GameObject Chassis;
 
+	public bool LMAO = false;
+
 	// Use this for initialization
 	void Start () {
 		Chassis.GetComponent<SpriteRenderer> ().color = new Color(Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f));
@@ -12,6 +14,8 @@ public class RandomCar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (LMAO) {
+			Chassis.GetComponent<SpriteRenderer> ().color = new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f));	
+		}
 	}
 }
